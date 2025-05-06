75f024d486c3effa25905961af01b038ecbb3bd1
aaryaVT <165612245+aaryaVT@users.noreply.github.com>
2025-05-06 11:58:43 -0400

    Update __init__.py

--- montecarlo/__init__.py
+++ montecarlo/__init__.py
@@ -1,8 +1,10 @@
 """The montecarlo package for PHYS3684"""
 
 # Add imports here
-from montecarlo import montecarlo
-from .bitstring import *
-from .isinghamiltonian import *
-from .metropolis import *
+#from montecarlo import montecarlo
+#from .bitstring import *
+#from .isinghamiltonian import *
+#from .metropolis import *
 
+from .functions import *
+from ._version import __version__
